e309fb390837f9658ef7c1ad7f8724e920931e8a
Kacper Barcik <45320966+kilofeq@users.noreply.github.com>
2023-04-22 14:42:38 +0200

    refactor: initial position

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -25,18 +25,11 @@ int32_t forces[2] = {0};
 
 ModbusMaster modbus;
 
-void preTransmission()
-{
-  digitalWrite(MAX485_RE_NEG, 1);
-  digitalWrite(MAX485_DE, 1);
-}
-
-void postTransmission()
-{
-  digitalWrite(MAX485_RE_NEG, 0);
-  digitalWrite(MAX485_DE, 0);
-}
-
+int torque_setting_position = 200;
+int encoder_setting_position = 391;
+int prev_encoder_value;
+int prev_torque = 0;
+int x_axis_position = 0;
 
 void setup()
 {
@@ -60,36 +53,31 @@ void setup()
   mygains[0].springGain = 100;//0-100
   Joystick.setGains(mygains);
   Joystick.begin();
-}
-
-int prev_encoder_value;
-int prev_torque = 0;
-int x_axis_position = 0;
-
-void loop()
-{
-  uint8_t result;
-  uint16_t data[6];
-  int torque_setting_position = 200;
-  int encoder_setting_position = 391;
-
-  // Encoder
-  result = modbus.readHoldingRegisters(encoder_setting_position, 1);
+  // Inital encoder value
+  uint8_t result = modbus.readHoldingRegisters(encoder_setting_position, 1);
   if (result == modbus.ku8MBSuccess)
   {
     int encoder_value = modbus.getResponseBuffer(0);
+    prev_encoder_value = encoder_value;
+  }
+}
+
+void loop()
+{
+  // Encoder
+  uint8_t result = modbus.readHoldingRegisters(encoder_setting_position, 1);
+  if (result == modbus.ku8MBSuccess)
+  {
+    int encoder_value = modbus.getResponseBuffer(0);
+    int encoder_position_change = encoder_value - prev_encoder_value;
     if (encoder_value != prev_encoder_value) {
-      int encoder_position_change = encoder_value - prev_encoder_value;
       // Handle new rotation
       if (encoder_position_change > 9000) {
-        int correct_encoder_position_change = ENCODER_CPR - encoder_position_change;
-        x_axis_position = x_axis_position + correct_encoder_position_change;
+        encoder_position_change = ENCODER_CPR - encoder_position_change;
       } else if (encoder_position_change < -9000) {
-        int correct_encoder_position_change = ENCODER_CPR + encoder_position_change;
-        x_axis_position = x_axis_position + correct_encoder_position_change;
-      } else {
-        x_axis_position = x_axis_position + encoder_position_change;
+        encoder_position_change = ENCODER_CPR + encoder_position_change;
       }
+      x_axis_position = x_axis_position + encoder_position_change;
       prev_encoder_value = encoder_value;
     }
   }
@@ -97,23 +85,32 @@ void loop()
   // FFB
   int torque = 0;
   // Endstop
-  if (x_axis_position + 1 > joystick_max_position) {
-    torque = -END_STOP_TORQUE;
-  } else if (x_axis_position - 1 -joystick_max_position) {
-    torque = END_STOP_TORQUE;
-  } else {
-    torque = 0;
-  }
   myeffectparams[0].springMaxPosition = joystick_max_position;
   myeffectparams[0].springPosition = x_axis_position;
   Joystick.setXAxis(x_axis_position);
   Joystick.setEffectParams(myeffectparams);
   Joystick.getForce(forces);
-  if (x_axis_position < joystick_max_position && x_axis_position > -joystick_max_position) {
+  if (x_axis_position + 1 > joystick_max_position) {
+    torque = -END_STOP_TORQUE;
+  } else if (x_axis_position - 1 -joystick_max_position) {
+    torque = END_STOP_TORQUE;
+  } else {
     torque = forces[0] / 255;
   }
   if (prev_torque != torque) {
     modbus.writeSingleRegister(torque_setting_position, torque);
   }
   prev_torque = torque;
+}
+
+void preTransmission()
+{
+  digitalWrite(MAX485_RE_NEG, 1);
+  digitalWrite(MAX485_DE, 1);
+}
+
+void postTransmission()
+{
+  digitalWrite(MAX485_RE_NEG, 0);
+  digitalWrite(MAX485_DE, 0);
 }
